18ee0429692fd176473742741373f47d74611884
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-07-09 19:32:14 +0530

    [Mayur] Refactor : Calculated wage till specified condition.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-// UC4
+// UC6
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck)
 {
@@ -22,10 +23,13 @@ function getWorkingHours(empCheck)
 }
 
 let totalEmpHours = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+let totalWorkingDays = 0;
+while (totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+  totalWorkingDays++;
   let empCheck = Math.floor(Math.random() * 10) % 3;
   totalEmpHours = totalEmpHours + getWorkingHours(empCheck);
 }
+
 let empWage = totalEmpHours * WAGE_PER_HOUR;
-console.log("Hours : " + totalEmpHours + " Employee Wage : " + empWage);
+console.log("Total Days : " + totalWorkingDays + ", Total Hours : " + totalEmpHours + ", Employee Wage : " + empWage);
