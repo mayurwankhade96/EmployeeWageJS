f92c55cf4bf3880b68ba3ed824611c0c15e4797c
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-07-09 19:18:24 +0530

    [Mayur] Refactor : Calculated wage for month considering 20 working days.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,9 +4,12 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
-function getWorkingHours(empCheck) {
-  switch (empCheck) {
+function getWorkingHours(empCheck)
+{
+  switch (empCheck)
+  {
     case IS_PART_TIME:
       return PART_TIME_HOURS;
 
@@ -18,7 +21,11 @@ function getWorkingHours(empCheck) {
   }
 }
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHours = getWorkingHours(empCheck);
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Hours : " + empHours + " Employee Wage : " + empWage);
+let totalEmpHours = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+{
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  totalEmpHours = totalEmpHours + getWorkingHours(empCheck);
+}
+let empWage = totalEmpHours * WAGE_PER_HOUR;
+console.log("Hours : " + totalEmpHours + " Employee Wage : " + empWage);
